fea57532a9f7a3769812a382b5679def3f43dcbd
arafatmansuri <mansuriarafat302@gmail.com>
2025-12-24 22:42:49 +0530

    Add send OTP controller for creating compnay

--- backend/src/controllers/companyController.ts
+++ backend/src/controllers/companyController.ts
@@ -1,6 +1,9 @@
+import bcrypt from "bcrypt";
 import { CookieOptions } from "express";
+import otpGenerator from "otp-generator";
 import z from "zod";
 import { Company } from "../models/companyModel";
+import { OTP } from "../models/otpModel";
 import { User } from "../models/userModel";
 import { Handler, StatusCode } from "../types";
 const companySchema = z.object({
@@ -82,6 +85,96 @@ export const createCompany: Handler = async (req, res) => {
         user,
         success: true,
       });
+    return;
+  } catch (error) {
+    res.status(StatusCode.ServerError).json({
+      message: "Something went wrong from our side",
+      success: false,
+      error,
+    });
+    return;
+  }
+};
+export const createCompanySendOTP: Handler = async (req, res) => {
+  try {
+    const parsedCompany = companySchema.safeParse(req.body);
+    if (!parsedCompany.success) {
+      res.status(StatusCode.InputError).json({
+        message: parsedCompany.error.issues[0].message,
+        success: false,
+      });
+      return;
+    }
+    const { companyEmail, companyName, companyPassword, companySize } =
+      parsedCompany.data;
+    const isCompanyExists = await Company.findOne({
+      $or: [{ companyEmail }, { companyName }],
+    });
+    if (isCompanyExists) {
+      res.status(StatusCode.DocumentExists).json({
+        message: "Company with name or email already exists",
+        success: false,
+      });
+      return;
+    }
+    const isOTPExists = await OTP.findOne({ companyEmail, type: "signup" })
+      .sort({ createdAt: -1 })
+      .limit(1);
+    const cookieOptions: CookieOptions = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      path: "/",
+      maxAge: 20 * 60000, // 20 minutes
+    };
+    if (isOTPExists) {
+      const otpCreatedTime = new Date(isOTPExists.createdAt);
+      if (new Date().getTime() - otpCreatedTime.getTime() <= 120000) {
+        await OTP.deleteMany({ companyEmail, type: "signup" });
+        const newOtp = await OTP.create({
+          companyEmail,
+          otp: isOTPExists.otp,
+          subject: "OTP for user signup",
+          companyPassword: bcrypt.hashSync(companyPassword, 10),
+          companyName,
+          type: "signup",
+        });
+        res
+          .cookie(
+            "otp_data",
+            { companyEmail: newOtp.companyEmail, type: "signup" },
+            cookieOptions
+          )
+          .status(200)
+          .json({ message: "OTP sent successfully" });
+        return;
+      }
+    }
+    const otp = otpGenerator.generate(6, {
+      lowerCaseAlphabets: false,
+      upperCaseAlphabets: false,
+      specialChars: false,
+    });
+    const newOtp = await OTP.create({
+      companyEmail,
+      otp:Number(otp),
+      subject: "OTP for user signup",
+      companyPassword: bcrypt.hashSync(companyPassword, 10),
+      companyName,
+      type: "signup",
+    });
+    if (!newOtp) {
+      res.status(500).json({ message: "OTP not generated" });
+      return;
+    }
+    res
+      .cookie(
+        "otp_data",
+        { companyEmail: newOtp.companyEmail, type: "signup" },
+        cookieOptions
+      )
+      .status(200)
+      .json({ message: "OTP sent successfully" });
     return;
   } catch (error) {
     res.status(StatusCode.ServerError).json({
