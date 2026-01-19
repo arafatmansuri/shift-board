00ac166e8eefcd29d98d06b4799c714c4b03cbeb
arafatmansuri <mansuriarafat302@gmail.com>
2026-01-19 19:50:43 +0530

    Update backend url

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,2 +1,3 @@
 // export const BACKEND_URL = "http://localhost:3000/api";
-export const BACKEND_URL = "https://apishift.secondbrain.services/api";
+// export const BACKEND_URL = "https://apishift.secondbrain.services/api";
+export const BACKEND_URL = "https://shift-board-83yy.onrender.com/api";
